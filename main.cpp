201c26d184536240ba32784425e12e217e0a43b4
Bodden007 <denis-bodrov@mail.ru>
2023-06-07 19:31:19 +0500

    added alocator

--- main.cpp
+++ main.cpp
@@ -3,60 +3,55 @@
 #include <memory>
 #include <memory_resource>
 
-// Создание аллокатора
-template <typename T>
-struct MyAllocator {
-	using value_type = T;
-
-	// Аллокатор дефолтный
-	MyAllocator() = default;
-
-	//Конструктор копирования аллокатора
-	template <typename U>
-	MyAllocator(const MyAllocator<U>&) {}
-
-	//Выделение пула памяти
-	void* operator new[](std::size_t n)
-	{
-		return ::operator new[](10);
-	}
+//// Создание аллокатора
+template<typename T, size_t pre_size>
+struct MyAllocator
+{
+	T* m_memory;
 	
-	// ??? превышение лимита  должнотвыбрасываться bad_alloc. НЕ РАБОТАЕТ!!!!
-	T* allocate(std::size_t n)
+	size_t m_position{ 0 };
+
+	MyAllocator()
 	{
-		if (n > 10)
-			throw std::bad_alloc();
-		return static_cast<T*>(::operator new(n * sizeof(T)));
+		m_memory = ::operator new(pre_size);
 	}
 
-	//Деструктор аллокатора
-	void deallocate(T* p, std::size_t) {
-		operator delete(p);
+	T* allocate(const size_t n)
+	{
+		if (m_position + n > pre_size)
+		{
+			throw std::bad_alloc{"no more memory!"};
+		}
+
+		const auto result = m_memory + m_position;
+		m_position += n;
+		return result;
 	}
+
 };
 
-//объявление функции factorial
-template <typename Allocator>
-void factorial(std::map <int, int, std::less<int>, Allocator >& factor);
+template<typename Allocator>
+void factorial(std::map<int, int, std::less<int>, Allocator> & factor);
 
 int main()
 {	
-	std::map <int, int, std::less<int>, MyAllocator<std::pair <const int, int>> > factor_alloc;
+	std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>, 10>> myMap;
 
-	factorial(factor_alloc);
+	std::map<int, int> myMap;
 
-	for (const auto f : factor_alloc)
+	/*factorial(myMap);*/
+
+	for (const auto f : myMap)
 	{
 		std::cout << f.first << " " << f.second << std::endl;
 	}
 }
 
-//определение функции factorial 
-// Надо ли здесь добовлять выделение памяти???
+
 template<typename Allocator>
 void factorial(std::map<int, int, std::less<int>, Allocator>& factor)
 {
-	for (int i = 0; i < 12; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		factor[i] = 1;
 
