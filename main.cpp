53840d442b3f23e30e76493c5685ebe897a2395f
Bodden007 <denis-bodrov@mail.ru>
2023-06-10 11:06:58 +0500

    Corrections 1006

--- main.cpp
+++ main.cpp
@@ -14,32 +14,31 @@ struct MyAllocator {
 		m_memory = static_cast<T*>(::operator new(pre_size * sizeof(T)));
 	}
 	
-	~MyAllocator() {};
+	~MyAllocator() 
+	{
+		::operator delete(m_memory);
+	};
 
 	template <typename U>
 	MyAllocator(const MyAllocator<U>&) {}
 
-	T* allocate(std::size_t n) {
+	T* allocate(const size_t n) {
 		
-		if (m_position+n > pre_size)
-		{
+		if (n > 10)
 			throw std::bad_alloc();
-		}
-			
-		const auto result = m_memory + m_position;
-		m_position += n;
-		return result;
+
+		return static_cast<T*>(::operator new(pre_size * sizeof(T)));
+		
 	}
 
 	void deallocate(T* p, std::size_t) {
-		operator delete(p);
+		
 	}
 
 private:
-	T* m_memory;
-	size_t m_position{ 0 };
-};
+	T* m_memory{nullptr};
 
+};
 
 template<typename Allocator>
 void factorial(std::map<int, int, std::less<int>, Allocator> & factor);
@@ -60,7 +59,7 @@ int main()
 template<typename Allocator>
 void factorial(std::map<int, int, std::less<int>, Allocator>& factor)
 {
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < 9; i++)
 	{
 		factor[i] = 1;
 
